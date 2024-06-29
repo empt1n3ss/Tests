4efce095376a803d665e6351c3d77de88b3fe8ac
n38757902 <n38757902@gmail.com>
2024-06-29 23:41:44 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-    public int add(String name, String number){
-        return 0;
+    private Map<String, String> contacts = new HashMap<>();
+
+    public int add(String name, String number) {
+        if (contacts.containsKey(name) && contacts.get(name).equals(number)) {
+            System.out.println("Вы пытаетесь добавить уже существующий контакт");
+        } else {
+            contacts.put(name, number);
+            System.out.println(contacts);
+            System.out.println("Количество контактов " + contacts.size());
+        }
+        return contacts.size();
     }
 }
