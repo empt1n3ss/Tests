e6cdca7fa95b414909ab6dcb71db30dea70f151e
n38757902 <n38757902@gmail.com>
2024-06-29 23:34:26 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBookTest.java
+++ src/main/java/org/example/PhoneBookTest.java
@@ -1,4 +1,15 @@
 package org.example;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd(){
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Name1", "111"));
+        assertEquals(1, phoneBook.add("Name1", "111"));
+        assertEquals(2, phoneBook.add("Name2", "222"));
+    }
 }
